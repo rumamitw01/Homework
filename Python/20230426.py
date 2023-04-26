2d34eec9143a76ad046ecf2c59b3b805b96061a9
琉見 <42371341+rumamitw01@users.noreply.github.com>
2023-04-26 16:57:50 +0800

    Update 20230426.py

--- Python/20230426.py
+++ Python/20230426.py
@@ -1,1 +1,44 @@
+#Function_Homework_01
+def WC(kg):
+    lb=kg*2.205
+    return lb
+i=float(input("Weight(Kilogram):"))
+print(f"Weight(Pound):{WC(i)}")
 
+#Function_Homework_02
+mb=10000
+day=6
+p=divmod(mb,day)
+print(f"Maximum Budget:${mb}")
+print(f"Days of travel:{day} days")
+print(f"Budget per day:${p[0]}")
+print(f"Remain Budget:${p[1]}")
+
+#Function_Homework_03
+f=input("Filename:")
+if f.lower().endswith("jpg"):
+    print("Correct!")
+else:
+    print("Wrong format!")
+
+#Function_Homework_04
+data=[["Name","S1","S2","S3","S4"],
+      ["Bill","34210","9000","186500","78900"],
+      ["May","23600","23900","127800","125000"],
+      ["Jack","145000","83400","100000","90000"]]
+for i in range(len(data)):
+    print(data[i][0].rjust(4),end=" ")
+    for j in range(1,len(data[i])-1):
+        if j==2:
+            print(data[i][j].rjust(6),end=" ")
+        else:
+            print(data[i][j].rjust(7),end=" ")
+    print(data[i][len(data)].rjust(7))
+
+#Function_Homework_05
+time="11:30:26"
+print(time)
+time=time.replace(":"," Hours ",1)
+time=time.replace(":"," Minutes ",1)
+time+=" Seconds"
+print(time)
