fd923e7c11b35e00718e84e615a3c5dc87c5c6b0
琉見 <42371341+rumamitw01@users.noreply.github.com>
2023-05-10 23:33:18 +0800

    Update 20230510.py

--- Python/20230510.py
+++ Python/20230510.py
@@ -35,8 +35,6 @@ while True:
     if len(x) <4:
         print('你輸入的並非四位數或有重複！')
         continue
-    elif temp =='給我答案':
-            print(string)
     elif temp==string:
         print("恭喜你猜對了!")
         break
@@ -50,31 +48,88 @@ while True:
         print(f"{A}A{B}B")
         print("還沒對，繼續")
 #Format_Homework_03
-a=input("請輸入生日")
+a=input("請輸入生日:")
 a_list=a.split("/")
-month_end={1:31,2:29,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
-star_sign_start={"Aries":,"Taurus":,"Gemini":,"Cancer":,"Leo":,"Virgo":,"Libra":,"Scorpio":,"Sagittarius":,"Capricorn":,"Aquarius":,"Pisces":}
-star_sign_end={"Aries":,"Taurus":,"Gemini":,"Cancer":,"Leo":,"Virgo":,"Libra":,"Scorpio":,"Sagittarius":,"Capricorn":,"Aquarius":,"Pisces":}
-'''牡羊座 Aries：3月21日~4月19日
-
-金牛座 Taurus：4月20日~5月20日
-
-雙子座 Gemini：5月21日~6月21日
-
-巨蟹座 Cancer：6月22日~7月22日
-
-獅子座 Leo：7月23日~8月22日
-
-處女座 Virgo：8月23日~9月22日
-天秤座 Libra：9月23日~10月23日
-
-天蠍座 Scorpio：10月24日~11月21日
-
-射手座 Sagittarius：11月22日～12月20日
-
-摩羯座 Capricorn：12月21日 ~ 1月20日
-
-水瓶座 Aquarius：1月21日 ~ 2月19日
-
-雙魚座 Pisces：2月20日 ~ 3月20日
-'''
+bir_month=int(a_list[0])
+bir_day=int(a_list[1])
+month_end=[31,29,31,30,31,30,31,31,30,31,30,31]
+sign_day=[20,19,21,20,21,22,23,23,23,23,24,23,22]
+sign_name=["魔羯座","水瓶座","雙魚座","白羊座","金牛座","雙子座","巨蟹座","獅子座","處女座","天秤座","天蠍座","射手座"]
+print(f"生日{bir_month}月{bir_day}日，是",end="")
+if bir_month>12 or bir_day>month_end[bir_month-1]:
+    print("輸入不存在的日期，無法轉換。")
+elif bir_day<sign_day[bir_month-1]:
+    print(sign_name[bir_month-1]+"。")
+elif bir_day>sign_day[bir_month-1] and bir_day<month_end[bir_month-1]:
+    print(sign_name[bir_month]+"。")
+else:
+    if bir_month<len(sign_name):
+        print(sign_name[bir_month]+"。")
+    else:
+        print(sign_name[bir_month-len(sign_name)]+"。")
+#Format_Homework_04
+kanji=["年","月","日"]
+a=input("請輸入年月日，(例:2021年1月1日):")
+for i in kanji:
+    a=a.replace(i,"/")
+a=a.rstrip("/")
+a_list=a.split("/")
+string=""
+for i in range(len(a_list)):
+    if i!=0:
+        if len(a_list[i])==1:
+            a_list[i]="0"+a_list[i]
+    string+=a_list[i]
+print(f"數字字串:{string}")
+#Read_and_Write_File_Homework_01
+import random
+w=["但我不能放歌","悄悄是別離的笙簫","夏蟲也為我沉默","沉默是今晚的康橋","悄悄的我走了","正如我悄悄的來","我揮一揮衣袖","不帶走一片雲彩"]
+n=random.randint(1,3)
+f=open("file.csv","a")
+for i in range(n):
+    m=random.randint(3,5)
+    p=random.sample(w,m)
+    print(" ".join(p),file=f)
+#Numpy_Homework_01
+import numpy as np
+jpy=np.array([13000,8000,15000,50000])
+twd=jpy*0.26
+discount=twd*0.98
+for i in range(len(discount)):
+    if discount[i]>=3000 and discount[i]<=4000:
+        print(f"第{i+1}間符合預算")
+    elif discount[i]<3000:
+        print(f"第{i+1}間低於預算")
+    else:
+        print(f"第{i+1}間高於預算")
+#Numpy_Homework_02
+import numpy as np
+A_w=np.array([40,60,70])
+B_w=np.array([60,50,65])
+A_h=np.array([150,168,176])
+B_h=np.array([180,170,140])
+BMI_A=A_w/((A_h/100)**2)
+BMI_B=B_w/((B_h/100)**2)
+print("A班學生BMI列表:")
+for i in BMI_A:
+    print(round(i,2))
+print("B班學生BMI列表:")
+for i in BMI_B:
+    print(round(i,2))
+print(f"A班BMI平均:{round(BMI_A.sum()/len(BMI_A),2)}")
+print(f"B班BMI平均:{round(BMI_B.sum()/len(BMI_B),2)}")
+print("以上數值皆僅取至小數點後第二位")
+#Numpy_Homework_03
+import numpy as np
+jpy=np.array([13000,8000,15000,50000])
+twd=jpy*0.26
+discount=twd*0.98
+d_b_f=(discount>=3000) & (discount<=4000)
+d_b_l=discount<3000
+for i in range(len(d_b_f)):
+    if d_b_f[i]:
+        print(f"第{i+1}間符合預算")
+    elif d_b_l[i]:
+        print(f"第{i+1}間低於預算")
+    else:
+        print(f"第{i+1}間高於預算")
